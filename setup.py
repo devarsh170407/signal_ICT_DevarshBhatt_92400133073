c05fd622ff34bbb06c3c477ec309601393926a17
devarsh170407 <devarshbhatt1747@gmail.com>
2025-09-06 18:19:53 +0530

    new

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="signal_ICT_DevarshBhatt_92400133073",
-    version="1.0.0",
+    version="2.0.0",
     author="DEVARSH BHATT",
     author_email="devarsh.bhatt128171@marwadiuniversity.ac.in",
     description="Python package for signal generation and operations",
