14a7caec98ce976309ada9cc887c8c58bab650ab
devarsh170407 <devarshbhatt1747@gmail.com>
2025-09-06 13:54:14 +0530

    new

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signal_ICT_DevarshBhatt_92400133073",
+    version="1.0.0",
+    author="DEVARSH BHATT",
+    author_email="devarsh.bhatt128171@marwadiuniversity.ac.in",
+    description="Python package for signal generation and operations",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib"
+    ],
+)
